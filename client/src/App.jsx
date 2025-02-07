cac1d36018239e1d1085758aed2f83fb8f22b979
Vasudev <79050603+Velstruck@users.noreply.github.com>
2025-02-07 23:02:56 +0530

    init blog feature event

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button } from './components/ui/button'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './Layout/Layout'
-import { RouteAddCategory, RouteCategoryDetails, RouteEditCategory, RouteIndex, RouteProfile, RouteSignIn, RouteSignUp } from './helpers/RouteName'
+import { RouteAddCategory, RouteBlog, RouteBlogAdd, RouteBlogEdit, RouteCategoryDetails, RouteEditCategory, RouteIndex, RouteProfile, RouteSignIn, RouteSignUp } from './helpers/RouteName'
 import { Index } from './pages/Index'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
@@ -10,6 +10,9 @@ import Profile from './pages/Profile'
 import AddCategory from './pages/categories/AddCategory'
 import CategoryDetails from './pages/categories/CategoryDetails'
 import EditCategories from './pages/categories/EditCategories'
+import AddBlog from './pages/blog/AddBlog'
+import BlogDetails from './pages/blog/BlogDetails'
+import EditBlog from './pages/blog/EditBlog'
 
 const App = () => {
   return (
@@ -18,9 +21,17 @@ const App = () => {
         <Route path={RouteIndex} element={<Layout />}>
           <Route index element={<Index />} />
           <Route path={RouteProfile} element={<Profile />} />
+          
+          {/* Blog category */}
           <Route path={RouteAddCategory} element={<AddCategory />} />
           <Route path={RouteCategoryDetails} element={<CategoryDetails />} />
           <Route path={RouteEditCategory()} element={<EditCategories />} />
+
+          {/* Blog */}
+          <Route path={RouteBlogAdd} element={<AddBlog />} />
+          <Route path={RouteBlog} element={<BlogDetails />} />
+          <Route path={RouteBlogEdit()} element={<EditBlog />} />
+
         </Route> 
         <Route element={<SignIn />} path={RouteSignIn} />
         <Route element={<SignUp />} path={RouteSignUp} />
