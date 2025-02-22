3264837a0a2b2ff3f8a6342f8eb777898b6d1614
Vasudev <79050603+Velstruck@users.noreply.github.com>
2025-02-23 00:08:01 +0530

    only admin fixed middleware

--- api/middleware/onlyadmin.js
+++ api/middleware/onlyadmin.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 export const onlyadmin = (req, res, next) => {
     try {
-        const  token  = req.cookies.access_token;
+        const  token  = req.cookies.token;
         if (!token) {
             return next(403, 'Unauthorized');
         }
