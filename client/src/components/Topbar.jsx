cb4ac6b134aab6769e6c6be1d2c05c9b8fd8abdd
Vasudev <79050603+Velstruck@users.noreply.github.com>
2025-02-24 20:33:08 +0530

    making responsive on mobiles

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '@/assets/images/logo-white.png'
 import { Button } from './ui/button'
 import { Link, Navigate, Route, useNavigate } from 'react-router-dom'
@@ -22,6 +22,7 @@ import { FiLogOut } from "react-icons/fi";
 import { removeUser } from '@/redux/user/user.slice';
 import { showToast } from '@/helpers/showToast';
 import { getEnv } from '@/helpers/getEnv';
+import { IoSearch } from "react-icons/io5";
 
 
 
@@ -29,6 +30,7 @@ const Topbar = () => {
   const user = useSelector(state => state.user)
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const [showSearch, setShowSearch] = useState(false)
 
   const handleLogout = async () => {
     try {
@@ -49,17 +51,26 @@ const Topbar = () => {
       showToast('error', err.message)
     }
   }
+
+  const toggleSearch = ()=>{
+    setShowSearch(!showSearch)
+  }
   return (
     <div className='flex justify-between items-center h-16 fixed w-full z-20 bg-white px-2.5 border-b'>
       <div>
         <Link to={RouteIndex}>
-          <img src={logo} />
+          <img src={logo} className='md:w-auto w-44' />
         </Link>
       </div>
       <div className='w-[500px]'>
-        <SearchBox />
+        <div className={`md:relative md:block absolute bg-white left-0 w-full md:top-0 top-16 md:p-0 p-5 ${showSearch ? 'block' : 'hidden'}`}>
+          <SearchBox />
+        </div>
       </div>
-      <div>
+      <div className='flex items-center gap-5'>
+        <button onClick={toggleSearch} type='button' className='md:hidden block'>
+          <IoSearch  size={25}/>
+        </button>
         {!user.isLoggedIn ?
           <Button className='rounded-full' asChild>
             <Link to={RouteSignIn}>
